2d6d69a929d8de63ca36b584f69c17e6df5adb2e
Jideofor <njusticej@gmail.com>
2021-02-17 22:33:35 +0100

    feat: add followers component

--- src/components/Followers.js
+++ src/components/Followers.js
@@ -1,9 +1,24 @@
 import React from 'react';
-import { GithubContext } from '../context/context';
+import { useGlobalContext } from '../context/context';
 import styled from 'styled-components';
 
 const Followers = () => {
-  return <h2>followers component</h2>;
+  const { githubFollowers } = useGlobalContext()
+  console.log(githubFollowers);
+  return <Wrapper>
+    <div className="followers">
+      {githubFollowers.map((follower, index) => {
+        const {avatar_url:img, html_url, login} = follower
+        return <article key={index}>
+          <img src={img} alt={login}/>
+          <div>
+            <h4>{login}</h4>
+            <a href={html_url}>{html_url}</a>
+          </div>
+        </article>
+      })}
+    </div>
+  </Wrapper>;
 };
 
 const Wrapper = styled.article`
